a4ca62edae2dc33ce43b166a587b4d610712be8e
Thrayz <taidihamza01@gmail.com>
2024-02-06 18:56:38 +0100

    small adjustments

--- app/src/main/java/com/example/currency/MainActivity.java
+++ app/src/main/java/com/example/currency/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.currency;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText amountEditText;
     private TextView resultTextView;
+    private RadioButton c1RadioButton;
+    private RadioButton c2RadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         amountEditText = findViewById(R.id.amount);
-        RadioButton c1RadioButton = findViewById(R.id.c1);
-        RadioButton c2RadioButton = findViewById(R.id.c2);
         resultTextView = findViewById(R.id.result);
+        c1RadioButton = findViewById(R.id.c1);
+        c2RadioButton = findViewById(R.id.c2);
+        Button convertButton = findViewById(R.id.convertButton);
 
-        c1RadioButton.setOnClickListener(v -> calculateAndDisplayResult(true));
-        c2RadioButton.setOnClickListener(v -> calculateAndDisplayResult(false));
+        convertButton.setOnClickListener(v -> convert());
     }
 
     @SuppressLint("SetTextI18n")
-    private void calculateAndDisplayResult(boolean isC1Selected) {
+    private void convert() {
         String amountStr = amountEditText.getText().toString();
 
         if (!amountStr.isEmpty()) {
             double amount = Double.parseDouble(amountStr);
-            double result = isC1Selected ? amount * 3 : amount / 3;
+            double result;
+            if (c1RadioButton.isChecked()) {
+                result = amount * 3;
+            } else {
+                result = amount / 3;
+            }
             resultTextView.setText(String.valueOf(result));
         } else {
             resultTextView.setText("Enter a valid amount");
